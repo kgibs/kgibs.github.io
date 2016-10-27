9528d6b2ebb65bd66ed3af0f365f8d1daafe25bb
Kelley <kelley@kelleygibson.ca>
2016-10-27 14:08:59 -0400

    Replace all remaining for of with forEach

--- pure-js-content-filters/dist/js/vanilla-filters.js
+++ pure-js-content-filters/dist/js/vanilla-filters.js
@@ -87,31 +87,9 @@ function philterContent(philtersContainer, resultsContainer, userOptions) {
     document.querySelector(options.clearTarget).addEventListener('click', function (e) {
       e.preventDefault();
       // clear all activated filters and reset results upon click of Clear Filters link
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
-
-      try {
-        for (var _iterator = Array.prototype.slice.call(document.querySelectorAll('.activate-philter'))[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var reset = _step.value;
-
-          reset.classList.remove('activate-philter');
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
-      }
-
+      Array.prototype.slice.call(document.querySelectorAll('.activate-philter')).forEach(function (item) {
+        item.classList.remove('activate-philter');
+      });
       toggleVisible();
     });
   }
@@ -128,33 +106,11 @@ function philterContent(philtersContainer, resultsContainer, userOptions) {
       activeResults[randomNum].classList.add('random');
 
       // hide all results except for randomly selected only
-      var _iteratorNormalCompletion2 = true;
-      var _didIteratorError2 = false;
-      var _iteratorError2 = undefined;
+      Array.prototype.slice.call(document.querySelectorAll('.philter-active')).forEach(function (item) {
+        item.classList.remove('philter-active');
+      });
 
-      try {
-        for (var _iterator2 = Array.prototype.slice.call(document.querySelectorAll('.philter-active'))[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-          var hideEm = _step2.value;
-
-          hideEm.classList.remove('philter-active');
-        }
-
-        // redirect to URL of randomly selected item
-      } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion2 && _iterator2.return) {
-            _iterator2.return();
-          }
-        } finally {
-          if (_didIteratorError2) {
-            throw _iteratorError2;
-          }
-        }
-      }
-
+      // redirect to URL of randomly selected item
       window.location = document.querySelector('.random a').getAttribute('href');
     });
   }
@@ -181,64 +137,47 @@ function philterContent(philtersContainer, resultsContainer, userOptions) {
     }
 
     // loop through results to show only those with a class of the activated filter(s)
-    var _iteratorNormalCompletion3 = true;
-    var _didIteratorError3 = false;
-    var _iteratorError3 = undefined;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
     try {
-      for (var _iterator3 = results[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-        var single = _step3.value;
+      var _loop = function _loop() {
+        var single = _step.value;
 
         var showThis = true;
 
-        var _iteratorNormalCompletion4 = true;
-        var _didIteratorError4 = false;
-        var _iteratorError4 = undefined;
-
-        try {
-          for (var _iterator4 = Array.prototype.slice.call(activeFilters)[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-            var filter = _step4.value;
-
-            if (single.classList.contains(filter)) {
-              showThis;
-            } else {
-              showThis = false;
-            }
+        Array.prototype.slice.call(activeFilters).forEach(function (item) {
+          if (single.classList.contains(item)) {
+            showThis;
+          } else {
+            showThis = false;
           }
-        } catch (err) {
-          _didIteratorError4 = true;
-          _iteratorError4 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion4 && _iterator4.return) {
-              _iterator4.return();
-            }
-          } finally {
-            if (_didIteratorError4) {
-              throw _iteratorError4;
-            }
-          }
-        }
+        });
 
         if (showThis) {
           single.classList.add('philter-active');
         } else {
           single.classList.remove('philter-active');
         }
+      };
+
+      for (var _iterator = results[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        _loop();
       }
 
       // if there are no results with the activated filter(s), show the warning / error message
     } catch (err) {
-      _didIteratorError3 = true;
-      _iteratorError3 = err;
+      _didIteratorError = true;
+      _iteratorError = err;
     } finally {
       try {
-        if (!_iteratorNormalCompletion3 && _iterator3.return) {
-          _iterator3.return();
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
         }
       } finally {
-        if (_didIteratorError3) {
-          throw _iteratorError3;
+        if (_didIteratorError) {
+          throw _iteratorError;
         }
       }
     }
